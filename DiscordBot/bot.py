b98fde8d3c7606038aefa677a2e325a6c6a03709
Andrea Dahl <ahdahl@stanford.edu>
2022-03-09 15:34:09 -0800

    printing table using matplotlib

--- DiscordBot/bot.py
+++ DiscordBot/bot.py
@@ -18,6 +18,7 @@ from matplotlib import dates as mpl_dates
 import networkx as nx
 from deep_translator import GoogleTranslator
 import dataframe_image as dfi
+from pandas.plotting import table
 
 
 PERSPECTIVE_SCORE_THRESHOLD = 0.80
@@ -211,9 +212,18 @@ class ModBot(discord.Client):
                     else:
                         author_count[message_author_name] += 1 
                     message_of_interest[flagged_message.content] = author_count
-            print("Check dictionary: ", message_of_interest)        
+            print("Check dictionary: ", message_of_interest) 
             freq_data = pd.DataFrame(message_of_interest)
-            dfi.export(freq_data,"table.png")         
+            fig, ax = plt.subplots(figsize=(12, 3)) 
+            ax.xaxis.set_visible(False)  
+            ax.yaxis.set_visible(False)  
+            ax.set_frame_on(False)  
+            tab = table(ax, freq_data, loc='upper right')  
+            tab.auto_set_font_size(False)
+            tab.set_fontsize(10) 
+            plt.savefig('table.png')        
+            # dfi.export(freq_data,"table.png") 
+            plt.clf()        
       
     async def on_raw_reaction_add(self, payload):
         guild = client.get_guild(payload.guild_id)
